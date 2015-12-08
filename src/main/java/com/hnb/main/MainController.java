c6fd1a7ba055383c5af26536c70d11207cd64173
skysoblue <coolbeat@naver.com>
2015-12-08 11:05:43 +0900

    .

--- src/main/java/com/hnb/main/MainController.java
+++ src/main/java/com/hnb/main/MainController.java
@@ -21,7 +21,7 @@ public class MainController {
 	
 	@RequestMapping(value = "/", method = RequestMethod.GET)
 	public String home(Locale locale, Model model) {
-		logger.info("Welcome home! The client locale is {}.", locale);
+		logger.info("메인컨트롤러 home 메소드 진입 {}.", locale);
 		
 		Date date = new Date();
 		DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
--- src/main/java/com/hnb/member/MemberController.java
+++ src/main/java/com/hnb/member/MemberController.java
@@ -1,5 +1,11 @@
 package com.hnb.member;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+
 public class MemberController {
+	private static final Logger logger = LoggerFactory.getLogger(MemberController.class);
+	
 
 }
